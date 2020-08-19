871d8a96203a92c93f5861cf35d24f5b5a74e6a6
Juli <julia.kukulies@gu.se>
2020-08-19 19:09:22 +0200

    WRF data

--- wrftp/get_wrftp.sh
+++ wrftp/get_wrftp.sh
@@ -2,7 +2,7 @@
 
 ## This bash script preprocesses data from a 9 km WRF downscaling over the Tibetan Plateau
 
-for y in {1979..2002}
+for y in {1980..2002}
 do
     for m in {05..09}
     do
@@ -11,7 +11,9 @@ do
 	echo "${DIR}"
 	if [ -d "${DIR}" ]; then
 	    # calculate monthly mean from hourly files 
-	    cdo -ensmean -select,name=u_tr_p,v_tr_p,r_v_p,Z_p,T_p ${DIR}wrfout_d01_TP9km_CF_${y}-${m}-*nc data/wrfout_TP9km_${y}_${m}.nc
+	    cdo -select,name=u_tr_p,v_tr_p,r_v_p,Z_p,T_p ${DIR}wrfout_d01_TP9km_CF_${y}-${m}-*nc data/selected.nc
+	    cdo timmean data/selected.nc wrfout_TP9km_CF_${y}_${m}.nc
+	    rm data/selected.nc
 	fi
     done
 done
