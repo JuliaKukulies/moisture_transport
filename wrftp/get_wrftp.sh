f180445f8fff8da6239405500702d8d52dc09daa
Juli <julia.kukulies@gu.se>
2020-08-19 13:44:46 +0200

    updated bash script for preprocessing of WRF data

--- wrftp/get_wrftp.sh
+++ wrftp/get_wrftp.sh
@@ -8,12 +8,12 @@ for y in {1979..2002}:
 do
     for m in {05..09}:
     do
-	DIR="CF${y}/${m}/"
+	DIR="/media/DataP3/PlevTP/CF${y}/${m}/"
 	# check if directory exists 
 	if [ -d "${DIR}" ]; then
 	    # calculate monthly mean from hourly files 
-	    cdo ensmean ${DIR}/wrfout_d01_TP9km_CF_${y}-${m}-*nc wrfout_TP9km_${y}_${m}.nc
+	    cdo ensmean ${DIR}/wrfout_d01_TP9km_CF_${y}-${m}-*nc data/wrfout_TP9km_${y}_${m}.nc
 	fi
     done
 done
-		  
+	     
