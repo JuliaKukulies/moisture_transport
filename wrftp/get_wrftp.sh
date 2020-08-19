b33b5958147335ee42619e665f796b06b02f41ce
Juli <julia.kukulies@gu.se>
2020-08-19 14:58:38 +0200

    get WRF data

--- wrftp/get_wrftp.sh
+++ wrftp/get_wrftp.sh
@@ -11,9 +11,7 @@ do
 	echo "${DIR}"
 	if [ -d "${DIR}" ]; then
 	    # calculate monthly mean from hourly files 
-	    cdo -select,name=u_tr_P,v_tr_p,r_v_p,Z_p,T_p ${DIR}wrfout_d01_TP9km_CF_${y}-${m}-*nc selected.nc
-	    cdo timmean selected.nc data/wrfout_TP9km_${y}_${m}.nc
-	    rm selected.nc
+	    cdo -ensmean -select,name=u_tr_p,v_tr_p,r_v_p,Z_p,T_p ${DIR}wrfout_d01_TP9km_CF_${y}-${m}-*nc data/wrfout_TP9km_${y}_${m}.nc
 	fi
     done
 done
