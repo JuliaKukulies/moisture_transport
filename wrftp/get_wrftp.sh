f5186fd989e64bc82b6113eceb61145a0098b5b5
Juli <julia.kukulies@gu.se>
2020-08-19 14:00:19 +0200

    updated bash script for preprocessing of WRF data

--- wrftp/get_wrftp.sh
+++ wrftp/get_wrftp.sh
@@ -3,18 +3,20 @@
 ## This bash script preprocesses data from a 9 km WRF downscaling over the Tibetan Plateau
 
 
-
-for y in {1979..2002}:
+for y in {1979..2002}
 do
-    for m in {05..09}:
+    for m in {05..09}
     do
-	DIR=/media/DataP3/PlevTP/CF${y}/${m}/
+	DIR="/media/DataP3/PlevTP/CF${y}/${m}/"
 	# check if directory exists
-	echo ${DIR}
+	echo "${DIR}"
 	if [ -d "${DIR}" ]; then
 	    # calculate monthly mean from hourly files 
 	    cdo ensmean ${DIR}/wrfout_d01_TP9km_CF_${y}-${m}-*nc data/wrfout_TP9km_${y}_${m}.nc
 	fi
     done
 done
-	     
+
+
+
+
