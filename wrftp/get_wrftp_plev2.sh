21c82191ccb65b90885520ec9d2b360aa94a5332
Juli <julia.kukulies@gu.se>
2020-08-24 10:30:04 +0200

    get WRF data from old location

--- wrftp/get_wrftp_plev2.sh
+++ wrftp/get_wrftp_plev2.sh
@@ -12,7 +12,7 @@ do
 	echo "${DIR}"
 	if [ -d "${DIR}" ]; then
 	    # calculate monthly mean from hourly files 
-	    cdo -select,name=u_tr_p,v_tr_p,r_v_p,Z_p,T_p ${DIR} post_wrfout_d01_${y}-${m}-*nc data/selected.nc
+	    cdo -select,name=u_tr_p,v_tr_p,r_v_p,Z_p,T_p ${DIR}post_wrfout_d01_${y}-${m}-*nc data/selected.nc
 	    cdo timmean data/selected.nc data/wrfout_TP9km_CF_${y}_${m}.nc
 	    rm data/selected.nc
 	fi
