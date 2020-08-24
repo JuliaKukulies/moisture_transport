65b7dfd2c519edb17a66c74f62a30a2c75f8b41a
Juli <julia.kukulies@gu.se>
2020-08-24 10:59:36 +0200

    get 2d vars from WRF data

--- wrftp/get_wrftp.sh
+++ wrftp/get_wrftp.sh
@@ -12,8 +12,8 @@ do
 	echo "${DIR}"
 	if [ -d "${DIR}" ]; then
 	    # calculate monthly mean from hourly files 
-	    cdo -select,name=u_tr_p,v_tr_p,r_v_p,Z_p,T_p ${DIR}wrfout_d01_TP9km_CF_${y}-${m}-*nc data/selected.nc
-	    cdo timmean data/selected.nc wrfout_TP9km_CF_${y}_${m}.nc
+	    cdo -select,name=WaterFlx,precip_g ${DIR}wrfout_d01_TP9km_CF_${y}-${m}-*nc data/selected.nc
+	    cdo timmean data/selected.nc wrfout_TP9km_CF_2D_${y}_${m}.nc
 	    rm data/selected.nc
 	fi
     done
