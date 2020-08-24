3574cb6f7529db2391f7a6dc381a118cc407aea1
Juli <julia.kukulies@gu.se>
2020-08-24 10:18:54 +0200

    get WRF data from old location

---
+++ wrftp/get_wrftp_plev2.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+## This bash script preprocesses data from a 9 km WRF downscaling over the Tibetan Plateau
+ 
+
+for y in {1983..2007}
+do
+    for m in {05..09}
+    do
+	DIR="/media/DataP2/PlevTP/CF${y}/${m}/"
+	# check if directory exists
+	echo "${DIR}"
+	if [ -d "${DIR}" ]; then
+	    # calculate monthly mean from hourly files 
+	    cdo -select,name=u_tr_p,v_tr_p,r_v_p,Z_p,T_p ${DIR}wrfout_d01_TP9km_CF_${y}-${m}-*nc data/selected.nc
+	    cdo timmean data/selected.nc data/wrfout_TP9km_CF_${y}_${m}.nc
+	    rm data/selected.nc
+	fi
+    done
+done
+
+
+
+
+
