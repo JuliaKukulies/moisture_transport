a07ec5335fd8d386d3102fcbb9cd89fe43998493
Juli <julia.kukulies@gu.se>
2020-08-27 09:32:32 +0200

    get wrf 2d,data2

--- wrftp/get_wrftp_plev2.sh
+++ wrftp/get_wrftp_plev2.sh
@@ -12,7 +12,7 @@ do
 	echo "${DIR}"
 	if [ -d "${DIR}" ]; then
 	    # calculate monthly mean from hourly files 
-	    cdo -select,name=WaterFlx, precip_g ${DIR}post_wrfout_d01_${y}-${m}-*nc data/selected.nc
+	    cdo -select,name=WaterFlx,precip_g ${DIR}post_wrfout_d01_${y}-${m}-*nc data/selected.nc
 	    cdo timmean data/selected.nc data/wrfout_TP9km_CF_2D_${y}_${m}.nc
 	    rm data/selected.nc
 	fi
