23b7c61a474d81f62da65584c7d114e507fbcab0
Juli <julia.kukulies@gu.se>
2020-11-20 09:07:00 +0100

    updated processing script

--- wrftp/get_wrftp_hourly.sh
+++ wrftp/get_wrftp_hourly.sh
@@ -17,7 +17,7 @@ do
 		echo "${DIR}"
 		if [ -d "${DIR}" ]; then
 	    	# select vars 
-	    		cdo -select,name=WaterFlx,precip_g ${DIR}wrfout_d01_TP9km_CF_${y}-${m}-${d}_${h}.nc data/hourly/wrfout_TP9km_CF_${y}-${m}-${d}-${h}.nc
+	    		cdo -select,name=r_v_p,r_solid_p,r_rain_p,r_cloud_p,u_rt_p,v_tr_p,Z_p,T_P ${DIR}post_wrfout_d01_TP9km_CF_${y}-${m}-${d}_${h}.nc data/hourly/wrfout_TP9km_CF_${y}-${m}-${d}-${h}.nc
 		fi
 		done
 	done
